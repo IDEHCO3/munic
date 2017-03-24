a83dca5e38c7611d7082469d2c83f7a63dde72ea
aluizio <aluizio777@gmail.com>
2017-03-24 18:55:33 -0300

    fixed a little bug in url

--- munic_2015/urls.py
+++ munic_2015/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import include, patterns, url
 from .views import *
 
 urlpatterns = patterns('',
-    url(r'^esfera-municipal/$', EsferaMunicipalList.as_view(), name="list"),
+    url(r'^esfera-municipal/?$', EsferaMunicipalList.as_view(), name="list"),
 )
